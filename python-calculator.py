925e02a2f67d777faa8629646d405e870e01ac64
Daniyal Imtiaz <daniyalimtiaz418@gmail.com>
2026-07-30 16:51:34 +0500

    Initial Commit : Added Python Calculator Project

---
+++ python-calculator.py
@@ -0,0 +1,26 @@
+#print hello world
+print ( "Hello World ! Daniyal Here , A Future Billionaire . See ya'")
+
+#build a calculator
+
+print ( "Welcome To Python Calculator")
+
+
+#1 . take numbers from user
+a = int(input ( " Please enter first number: "))
+b = int(input ( " Please enter second number: " ))
+
+
+#doing operations
+sum = a + b
+subtract = a-b
+product = a * b
+division = a / b
+
+print ( "Here are your answers :")
+print ( "1. Sum : ", sum )
+print ( "2. Subtract : ", subtract )
+print ( "3. Product : ", product )
+print ( "4. Division : ", division )
+
+print ( "Thanks for using :)")
